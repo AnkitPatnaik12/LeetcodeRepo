36c452388f1329b4167ec4edbb468df38eeef969
AnkitPatnaik12 <68083741+AnkitPatnaik12@users.noreply.github.com>
2022-10-13 01:38:05 +0530

    Time: 9 ms (28.54%), Space: 80.4 MB (56.80%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,30 +1,22 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int gas1=0,prevgas=0,start=0,x=0,count=0;
-         int sum = 0, n = gas.length;
+       int sum = 0, n = gas.length;
         for(int i=0;i<n;i++) {
             sum += gas[i]-cost[i];
         }
         if(sum < 0) return -1;
-        while(start<gas.length)
-        {   
-            if(cost[x]>gas[x]+prevgas)
-            {   
-                start=x+1;
-                x=start;
-                count=0;
-                prevgas=0;
+		
+        int gasInTank = 0, start = 0;
+        for(int i=0;i<n;i++) {
+            gasInTank += gas[i]-cost[i];
+            // if we are not able to reach next station from i, 
+            if(gasInTank < 0) {
+                start = i+1;
+                gasInTank = 0;
             }
-            else
-            {
-                gas1=gas[x]+prevgas-cost[x];
-                prevgas=gas1;
-                count++;
-                x=(x+1)%gas.length;
-            }
-            if(count==gas.length)
-                return start;
         }
-        return -1;
+        
+        return gasInTank >= 0 ? start : -1; // we can as well just return "start" as we know circular route is possible
     }
+
 }
