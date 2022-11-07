60a9cf0f8da4ae68caae2148809ecdcfeec968f5
AnkitPatnaik12 <68083741+AnkitPatnaik12@users.noreply.github.com>
2022-11-07 14:02:54 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (46.74%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,14 +1,26 @@
 class Solution {
     public int maximum69Number (int num) {
-        String s=""+num;
-        int i=0;
-        while(i<s.length()&&s.charAt(i)!='6')
+        int arr[]=new int[5];
+        int p=num;
+        for(int i=4;i>=0;i--)
         {
-            i++;
-            
+            arr[i]=p%10;
+            p=p/10;
         }
-        if(i<s.length())
-            s=s.substring(0,i)+"9"+s.substring(i+1,s.length());
-        return Integer.parseInt(s);
+        for(int i=0;i<5;i++)
+        {
+            if(arr[i]!=0&&arr[i]==6)
+            {   arr[i]=9;
+                break;
+            }
+        }
+        int res=0;
+        for(int i=0;i<5;i++)
+        {
+            if(arr[i]!=0)
+                res=res*10+arr[i];
+        }
+        return res;
     }
+        
 }
